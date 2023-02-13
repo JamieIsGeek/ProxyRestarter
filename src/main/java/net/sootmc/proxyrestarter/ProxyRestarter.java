f2f26855fb7b22f6eed60e4506218c99a74c4798
JamieOnSootMC <jamie@sootmc.net>
2023-02-13 09:35:08 +0000

    Initial commit

---
+++ src/main/java/net/sootmc/proxyrestarter/ProxyRestarter.java
@@ -0,0 +1,16 @@
+package net.sootmc.proxyrestarter;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class ProxyRestarter extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
